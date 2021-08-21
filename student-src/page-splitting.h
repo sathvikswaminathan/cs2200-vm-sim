25023d79b4e0acaf4b9e9d49da1edefa9291f05e
Sathvik <sathvikswaminathan@gmail.com>
2021-08-21 16:36:00 +0530

    vm address splitting done

---
+++ student-src/page-splitting.h
@@ -0,0 +1,10 @@
+/* Step - 1 */
+
+#ifndef _STUDENT_PAGE_SPLITTING_H_
+#define _STUDENT_PAGE_SPLITTING_H_
+
+#define VADDR_PAGENUM(addr) ((addr) / (page_size))
+
+#define VADDR_OFFSET(addr) ((addr) % (page_size))
+
+#endif/*_STUDENT_PAGE_SPLITTING_H_*/
